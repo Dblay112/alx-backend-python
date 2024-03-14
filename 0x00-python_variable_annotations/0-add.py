2e7772258277144b596824d921dbccc67f35da59
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-03-14 20:20:29 +0000

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,16 +1,17 @@
-mport the necessary module.
+#!/usr/bin/python3
+""""
+Import the necessary module.
 """
 
 from typing import float
 
 def add(a: float, b: float) -> float:
-      """function that sum of two floating-point numbers.
+  """function that sum of two floating-point numbers.
 
-        Args:
-                  a: first floating-point number
-                        b: second floating-point number
-                          Returns:
-                                    sum of a and b (float).
-                                      """
-                                        return a + b
-
+  Args:
+      a: first floating-point number
+      b: second floating-point number
+  Returns:
+      sum of a and b (float).
+  """
+  return a + b
