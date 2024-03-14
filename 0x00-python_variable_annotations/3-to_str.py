080a0e3d34fd745f3f149b4cfe7c060f534c23e3
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-03-14 21:01:19 +0000

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""""
+Import the necessary module.
+"""
+
+
+def to_str(n: float) -> str:
+    """function that converts a float to its string representation.
+
+    Args:
+        n: float to convert to a string (float).
+
+    Returns:
+        string representation of n (str).
+    """
+    return str(n)
