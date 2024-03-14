1e60940e4792955489fb6f572a6d99abaa76144d
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-03-14 18:16:34 +0000

    fixed pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Creates a tuple from a string key and an 
+    """Creates a tuple from a string key and an
     integer/float value, returning the squared value.
 
     Args:
@@ -14,7 +14,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         v: The integer or float value (Union[int, float]).
 
     Returns:
-        A tuple containing the string key and the 
-        square of the value as a float (Tuple[str, float]).
+    A tuple containing the string key and the
+    square of the value as a float (Tuple[str, float]).
     """
     return k, v**2
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,26 +6,19 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """function that creates a new function that 
+    """function that creates a new function that
     multiplies a float by the given multiplier.
 
     Args:
-    multiplier: The float value to be used as 
+    multiplier: The float value to be used as
     the multiplier
 
     Returns:
-        a function
+    a function
     """
+    
+
     def innerFunction(number: float) -> float:
-    """Inner function that performs the multiplication.
-
-        Args:
-            number: The float to be multiplied by
-            the outer multiplier
-
-        Returns:
-           number and the outer multiplier multiplied together
-        """
-        return number * multiplier
-
-    return innerFunction
+    """Inner function that performs the multiplication"""
+    return number * multiplier
+return innerFunction
