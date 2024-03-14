11c20b3ae7941b72bd46e9081cd6dcb7b370a47b
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-03-14 20:47:12 +0000

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""
+Import the necessary module.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+  """Concatenates two strings.
+
+  Args:
+      str1: The first string (str).
+      str2: The second string (str).
+
+  Returns:
+      The concatenated string (str).
+  """
+  return str1 + str2
