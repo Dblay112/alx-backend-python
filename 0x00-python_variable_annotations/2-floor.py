bd53823a3146e9784545802b8d103ba657918951
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-03-14 20:55:29 +0000

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""""
+Import the necessary module.
+"""
+
+
+def floor(n: float) -> int:
+   """Return floor of a floating-point number.
+
+   Args:
+       n: floating-point number to round down (float).
+
+   Returns:
+       largest integer less than or equal to n (int).
+   """
+   return int(math.floor(n))
