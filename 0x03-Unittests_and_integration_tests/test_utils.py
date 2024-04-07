43e81627a03f6de0bfe743e649ca6f8043ba9b78
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-07 11:20:28 +0000

    fixed pycode errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,15 +8,17 @@ from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json, memoize
 
 
-class TestAccessNestedMap)unittest.Testcase):
-    """class to test nested map"""
-    @parameterize.expand([
-        ({"a":1}, ("a"), 1),
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    class to test accessnestedmap
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-        ])
-
-    def test_access_nested_map(self, nested_map, path, expected_value):
-        """test that the method returns what it is supposed to."""
-       mapped = access_nested_map(nested_map, path)
-       self.assertEqual(mapped, expected_value)
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        function to test the accessnested map
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
